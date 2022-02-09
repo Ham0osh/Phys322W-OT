e93192537ddca1d7e269ff01d8545f05ef8a5830
Hamoosh <hamish_johnson@sfu.ca>
2022-02-09 06:06:02 +0000

    improved the local max func

--- tool.py
+++ tool.py
@@ -5,29 +5,57 @@ from scipy.integrate import solve_ivp
 import scipy.interpolate as interpolate
 
 
-def local_max(arr, N = 3):
-    '''find local maximums of an array where local is defined as N points on either side'''
+def local_max(arr, N = 2, strict = False):
+    '''find local maximums of an array where local is defined as M points on either side, if strict is true
+    then it will follow this process exactly if strict is false it will also count local maxes that are at least
+    one space from the edge if they satisfy the requirement within the remaining array'''
     local_maxs = []
+    M = int(N/2)
     
     #loop through the array
-    i = N
+    if not strict:
+        i = 1
+
+    else:
+        i = M
+
     indexes = []
     
-    while i < len(arr) - 1 - N:
+    while i < len(arr) - 1:
         
         iterate = 1
         #flag
         local_max = True
         
-        for j in range(N):
-            if arr[i] < arr[i + j]:
-                local_max = False
-                iterate = j
-                break
-                
-            elif arr[i] < arr[i - j]:
-                local_max = False
-                break
+        for j in range(M):
+            try:
+                #will make index error when your with M of the edges so except index error
+                if arr[i] < arr[i + j]:
+                    local_max = False
+                    iterate = j
+                    break
+
+            except IndexError:
+                if strict:
+                    #reproduce old behaviour
+                    local_max = False
+                    break
+                #continue on to looking in the opposite direction
+                else:
+                    pass
+            
+            try:
+                if arr[i] < arr[i - j]:
+                    local_max = False
+                    break
+
+            except IndexError:
+                if strict:
+                    local_max = False
+                    break
+                else:
+                    pass
+
             
         if local_max:
             local_maxs.append(arr[i])
@@ -55,3 +83,12 @@ def unique_maxs(y: np.array, N = 5, error_tol = 1e-3):
             unique_maxs.append(maxs[i])
 
     return unique_maxs
+
+
+def noise(func, sigma = 0):
+    '''adds gaussian noise with standard deviation sigma to any function'''
+    def noisy_func(*args, **kwargs):
+        modified = func(*args, **kwargs)
+        modified += np.random.normal(scale = sigma, size = len(modified))
+        return modified
+    return noisy_func
